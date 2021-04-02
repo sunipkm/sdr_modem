bf3084d34ea83808e7103b4eb0142f9b4a367a74
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2021-04-02 00:54:04 -0400

    Showing the phy window

--- src/guimain.cpp
+++ src/guimain.cpp
@@ -322,6 +322,9 @@ int main(int, char **)
 
             ImGui::Checkbox("AD9361 Configuration Window", &show_phy_win);
 
+            if (show_phy_win)
+                PhyWin(&show_phy_win);
+
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             ImGui::End();
         }
